da8041d4486fba94c1c57b2df354420f92e485a5
newarmy <newarmy08@163.com>
2017-03-25 21:24:17 +0800

    ios slash quetion

--- rem-example/src/list/initheight.js
+++
@@ -1,9 +0,0 @@
-define(['zepto'], function ($) {
-	return function () {
-		$(document).ready(function() {
-			var headerH = $('header').height();
-			var winH = $(window).height();
-			$('.circle-list').css('height', (winH-headerH)+'px')
-		});
-	};
-});
--- rem-example/src/quickRecommend/linkTo.js
+++
@@ -1,8 +0,0 @@
-define(['zepto'], function ($) {
-	return function (obj) {
-		$('.complete-btn').on('click', function(e) {
-			e.preventDefault();
-			var arr = obj.selIdArr;
-		});
-	};
-});
